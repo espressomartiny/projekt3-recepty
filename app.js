5b7a170d19c24791aabf74a0cd6b86dd6391ac90
Martina Vokáčová <martinavokacova@gmail.com>
2022-04-30 14:57:53 +0200

    FCE filtrování dle kategorie

--- app.js
+++ app.js
@@ -18,44 +18,61 @@ recept-hodnoceni, recept-nazev, recept-popis.
 6) Poslední vybraný recept ulož do Local Storage, aby se při novém otevření aplikace načetl.
 */
 
-
 // 1)
 
 vygenerujSeznamReceptu(recepty);
 
 function vygenerujSeznamReceptu(poleReceptu) {
-poleReceptu.forEach(recept => {
-
-let receptDiv = document.createElement('div');
-receptDiv.className = 'recept';
-let obrazekReceptu = document.createElement('div');
-obrazekReceptu.className = 'recept-obrazek';
-let obrazek = document.createElement('img');
-obrazek.src = recept.img;
-obrazek.alt = 'obrázek pokrmu';
-let nazevReceptu = document.createElement('div');
-nazevReceptu.className = 'recept-info';
-nazev = document.createElement('h3');
-nazev.innerText = recept.nadpis;
-document.getElementById('recepty').appendChild(receptDiv);
-receptDiv.appendChild(obrazekReceptu);
-receptDiv.appendChild(nazevReceptu);
-obrazekReceptu.appendChild(obrazek);
-nazevReceptu.appendChild(nazev);
-
-});
+  poleReceptu.forEach((recept) => {
+    let receptDiv = document.createElement("div");
+    receptDiv.className = "recept";
+    let obrazekReceptu = document.createElement("div");
+    obrazekReceptu.className = "recept-obrazek";
+    let obrazek = document.createElement("img");
+    obrazek.src = recept.img;
+    obrazek.alt = "obrázek pokrmu";
+    let nazevReceptu = document.createElement("div");
+    nazevReceptu.className = "recept-info";
+    nazev = document.createElement("h3");
+    nazev.innerText = recept.nadpis;
+    document.getElementById("recepty").appendChild(receptDiv);
+    receptDiv.appendChild(obrazekReceptu);
+    receptDiv.appendChild(nazevReceptu);
+    obrazekReceptu.appendChild(obrazek);
+    nazevReceptu.appendChild(nazev);
+  });
 }
 
 // 2)
 
 function hledej() {
-    let vstup = (document.getElementById('hledat').value);
-    let nalezeneRecepty = recepty.filter(value => {
-        return (
-            value.nadpis.toLowerCase().includes(vstup.toLowerCase())
-        );
-    });
-    console.log(nalezeneRecepty)
+  let vstup = document.getElementById("hledat").value;
+  let nalezeneRecepty = recepty.filter((value) => {
+    return value.nadpis.toLowerCase().includes(vstup.toLowerCase());
+  });
+  console.log(nalezeneRecepty);
+}
 
-   }
+//3)
 
+function filtruj() {
+  document.getElementById("recepty").innerText = "";
+  let chod = document.getElementById("kategorie");
+  if (chod.value === "Snídaně") {
+    vygenerujSeznamReceptu(recepty.filter(snidej));
+  } else if (chod.value === "Hlavní jídlo") {
+    vygenerujSeznamReceptu(recepty.filter(obedvej));
+  } else if (chod.value === "Dezert") {
+    vygenerujSeznamReceptu(recepty.filter(dezertuj));
+  }
+}
+
+function snidej(jidlo) {
+  return jidlo.kategorie === "Snídaně";
+}
+function obedvej(jidlo) {
+  return jidlo.kategorie === "Hlavní jídlo";
+}
+function dezertuj(jidlo) {
+  return jidlo.kategorie === "Dezert";
+}
